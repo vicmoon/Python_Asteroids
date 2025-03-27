64be6535a880fa0f1cd3ddfcfd61b52edc636a11
Victoria Munteanu <munteanuvictoria1@gmail.com>
2025-03-27 11:21:46 +0100

    A: first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Asteroids")
+
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+            
+            screen.fill('black')
+            pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
